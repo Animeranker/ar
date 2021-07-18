950be784317387bf01f78291461ab4294db121db
Animeranker <76884376+Animeranker@users.noreply.github.com>
2021-07-18 16:18:07 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -446,7 +446,7 @@ function initTowhub() {
         $(".fixed-scroll-column-share-container").slideToggle(400);
     });
     $(".share-container").share({
-        networks: ['facebook', 'pinterest', 'twitter', 'reddit']
+        networks: ['facebook', 'pinterest', 'twitter', 'whatsapp']
     });
     var shrcn = $(".share-container");
     function showShare() {
